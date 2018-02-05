4dcb480a87ac8120f03a965277e92efe4ce49f76
wangshuang <wangshuang@memect.co>
2018-02-05 17:28:00 +0800

    [feature]增加了自定义脚本文件

---
+++ .bin/scripts/fuzzy_lock.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Take a screenshot
+scrot /tmp/screen.png
+
+# Pixellate it 10x
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+
+# add lock icon
+[[ -f $1  ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
+
+# pause  neteasy-cloud-music
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.cloudmusic /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Pause
+
+# Lock screen with this image
+i3lock -i /tmp/screen.png
+
+# Delete image 
+rm /tmp/screen.png
+
+# Turn the screen off after a delay.
+sleep 60
+if [ $(pgrep -c i3lock) -ne 0 ]
+  then
+    xset dpms force off
+fi
+
+# resume neteasy-cloud-music
+# dbus-send --print-reply --dest=org.mpris.MediaPlayer2.cloudmusic /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Play
