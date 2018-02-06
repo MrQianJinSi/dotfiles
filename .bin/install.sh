937f5b67c57d9160595e549abb063e51173d1cb4
wangshuang <wangshuang@memect.co>
2018-02-06 10:37:56 +0800

    [feature]add ranger related files

---
+++ .bin/install.sh
@@ -0,0 +1,20 @@
+# ranger preview file
+ranger_related = ('caca-utils',  # img2txt preview images
+          'atool', # tarball metainfo
+          'w3m', # html
+          'mediainfo', # prview mediafile metainfo
+          'highlight', # code
+          'pdftotext', # pdf
+          'transmission-show', # bit-torrent metainfo
+          'catdoc', # .doc 
+          'docx2txt', # .docx 
+          'xlsx2csv', # .xlsx
+          'xls2csv', # .xls
+          'unrar', # .rar
+          'xsel' # filename copy and paste
+          )
+
+# vim style application
+vim_style = ('sxiv', # image brownser
+             'zathura' # pdfviewer
+)
